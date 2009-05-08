bb4a86dd6f67b27a7c6464a0fc2922af163d65c7
Bryan Woods <bryanwoods4e@bryan-woodss-macbook-pro.local>
2009-05-08 18:54:15 -0400

    Adding right_aws

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   config.gem "rspec", :lib => false, :version => ">= 1.2.0"
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
   config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "right_aws"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
