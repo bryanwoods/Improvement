78d654115a5ac7628cb590e60bb19ce53bc48ab4
Bryan Woods <bryanwoods4e@bryan-woodss-macbook-pro.local>
2009-05-11 18:49:26 -0400

    S3 for file uploads and preliminary AJAX readying

--- app/controllers/yes_votes_controller.rb
+++ app/controllers/yes_votes_controller.rb
@@ -1,7 +1,7 @@
 class YesVotesController < ApplicationController
- def create
-   @poll = Poll.find(params[:poll_id])
-   @yes_vote = @poll.yes_votes.create!(params[:yes_vote])
-   redirect_to @poll
- end
+  def create
+    @poll = Poll.find(params[:poll_id])
+    @yes_vote = @poll.yes_votes.create!(params[:yes_vote])
+    redirect_to @poll
+  end
 end
