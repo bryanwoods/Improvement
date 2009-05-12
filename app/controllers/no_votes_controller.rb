72719bcf33c65e84b80fd9fdd5102f6d2c9ff6db
Bryan Woods <bryanwoods4e@bryan-woodss-macbook-pro.local>
2009-05-12 11:08:57 -0400

    Add some ajax

--- app/controllers/no_votes_controller.rb
+++ app/controllers/no_votes_controller.rb
@@ -2,6 +2,10 @@ class NoVotesController < ApplicationController
   def create
     @poll = Poll.find(params[:poll_id])
     @no_vote = @poll.no_votes.create!(params[:no_vote])
-    redirect_to @poll
+    flash[:notice] = "Suck on a No Vote!"
+    respond_to do |format|
+      format.html { redirect_to @poll }
+      format.js
+    end
   end
 end
--- app/controllers/yes_votes_controller.rb
+++ app/controllers/yes_votes_controller.rb
@@ -2,6 +2,10 @@ class YesVotesController < ApplicationController
   def create
     @poll = Poll.find(params[:poll_id])
     @yes_vote = @poll.yes_votes.create!(params[:yes_vote])
-    redirect_to @poll
+    flash[:notice] = "Upvote Attack!"
+    respond_to do |format|
+      format.html { redirect_to @poll }
+      format.js
+    end
   end
 end
