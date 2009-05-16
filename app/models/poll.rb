6f0ee0a2b937fd666c841caebe0bf5fecf5aa269
robertjwhitney <afinitetime@gmail.com>
2009-05-16 10:37:04 -0400

    pulling in bryan version

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -30,4 +30,32 @@ class Poll < ActiveRecord::Base
    
    validates_presence_of :title
    attr_accessible :before_pic, :title, :description, :after_pic
+   
+   def current_yes
+     current_yes = @polls.last.yes_votes.count
+   end
+
+   def current_no
+     current_no = @polls.last.no_votes.count
+   end
+
+   def current_rating
+     if current_yes > current_no
+       "UPGRADE"
+     else
+       "NOT AN UPGRADE"
+     end
+   end
+
+   def total_votes
+     total_votes = current_yes + current_no
+   end
+
+   def yes_percent
+     yes_percent = (current_yes.to_f / total_votes.to_f * 100).to_int
+   end
+
+   def no_percent
+     no_percent = (current_no.to_f / total_votes.to_f * 100).to_int
+   end
 end
